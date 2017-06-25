c1d75c990ae0f93a63480553fc95f8a1a13ce2d6
laurent russier <lrussier@xebia.fr>
2017-06-25 11:44:10 +0200

    Add Junit 5

--- app/src/test/kotlin/meetup/russier/fr/meetup/NewsPresenterTest.kt
+++ app/src/test/kotlin/meetup/russier/fr/meetup/NewsPresenterTest.kt
@@ -1,11 +1,14 @@
 package meetup.russier.fr.meetup
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.Test
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
 
+@DisplayName("class test")
 class NewsPresenterTest {
 
     @Test
+    @DisplayName("super test")
     internal fun name() {
         assertThat(true).isTrue()
     }
